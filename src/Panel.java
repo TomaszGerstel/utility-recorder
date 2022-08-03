428dd1a5c34a38051ef2c238ae05e2326a29cf77
Tomasz <gerstel.tomasz@gmail.com>
2022-08-03 15:39:20 +0200

    looking for something

--- src/Panel.java
+++ src/Panel.java
@@ -1,7 +1,17 @@
 import javax.swing.*;
+import javax.swing.plaf.TableHeaderUI;
+import javax.swing.table.*;
+import javax.swing.text.TableView;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Panel implements ActionListener {
@@ -10,7 +20,7 @@ public class Panel implements ActionListener {
 //    JToolBar jtb;
 //    JPopupMenu jpu;
 
-    public Panel() throws IOException {
+    public Panel() throws IOException, ParseException {
 
         JFrame jfrm = new JFrame("Utility Recorder");
 //        jfrm.setLayout(new FlowLayout());
@@ -42,7 +52,7 @@ public class Panel implements ActionListener {
                     public void run() {
                         try {
                             new Panel();
-                        } catch (IOException e) {
+                        } catch (IOException | ParseException e) {
                             e.printStackTrace();
                         }
                     }
@@ -182,44 +192,95 @@ public class Panel implements ActionListener {
 
 class DataPanel extends JPanel  {
 
-    StringBuilder text;
+    ArrayList<RecordModel> records;
     String line = "";
     File data;
+    String[] headers = {"Date", "Value", "Consumption"};
+    ArrayList<String> headers2 = new ArrayList<String>();
 
-    public DataPanel() throws IOException {
-        text = new StringBuilder();
+
+    public DataPanel() throws IOException, ParseException {
+//        text = new StringBuilder();
+        this.setLayout(new BorderLayout());
+        records = new ArrayList<>();
         data = new File("data_utility.txt");
-        this.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                try {
-                    loadData();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-            }
-        });
         loadData();
     }
 
-    public void loadData() throws IOException {
+    public void loadData() throws IOException, ParseException {
+//        enters = new String[][];
         System.out.print("load data");
         if(data.exists()) {
             BufferedReader br = new BufferedReader(new FileReader(data));
             while ((line = br.readLine()) != null) {
-                text.append("\r");
-                text.append(line);
+                SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+//                Pattern datePattern = Pattern.compile("\\d{2}-\\d{2}-\\d{4}");
+//                Matcher dateMatcher = datePattern.matcher(line);
+//                String dateFromLine = "";
+////                String dateFromLine = datePattern.matcher(line).group(1);
+//                if(dateMatcher.find()) dateFromLine = dateMatcher.group(0);
+//                System.out.print(dateFromLine);
+                Date date = formatter.parse(line);
+                RecordModel rec = new RecordModel(date, 28.0f);
+                records.add(rec);
+//                text.append(line);
             }
             br.close();
         }
-        else text = new StringBuilder("File with data doesn't exist." +
-                "Set up a new utility recorder or load data from a file from an external location.");
-        JTextArea textArea = new JTextArea(text.toString(), 25, 35);
-        add(textArea);
+//        else text = new StringBuilder("File with data doesn't exist." +
+//                "Set up a new utility recorder or load data from a file from an external location.");
+        DefaultTableModel tableModel = new DefaultTableModel();
+        tableModel.addColumn("Date");
+        tableModel.addColumn("State");
+        tableModel.addColumn("Consumption");
+        tableModel.addRow(headers);
+        JTable table = new JTable(tableModel);
+        table.setPreferredScrollableViewportSize(new Dimension(600, 100));
+        table.setFillsViewportHeight(true);
+        TableColumn col1 = new TableColumn(0, 21);
+
+
+        table.setEnabled(false);
+        table.setRowSelectionAllowed(true);
+
+
+//        table.setBounds(30, 40, 200, 600);
+//        table.setSize(600,300);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        float consumption = 0;
+        float lastValue = 0;
+        for(RecordModel rm : records) {
+            consumption = rm.getValue() - lastValue;
+            lastValue = rm.getValue();
+            tableModel.addRow(new Object[]{formatter.format(rm.getDate()), rm.getValue(), consumption});
+        }
+
+//        JTextArea textArea = new JTextArea(text.toString(), 25, 35);
+//        add(textArea);
+
+        add(new ScrollPane().add(table));
+
+    }
+
+    public class SimpleHeaderRenderer extends JLabel implements TableCellRenderer {
+
+        public SimpleHeaderRenderer() {
+            setFont(new Font("Consolas", Font.BOLD, 14));
+            setForeground(Color.BLUE);
+            setBorder(BorderFactory.createEtchedBorder());
+        }
+
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value,
+                                                       boolean isSelected, boolean hasFocus, int row, int column) {
+            setText(value.toString());
+            return this;
+        }
     }
 
 }
+
+
 
 class DiagramPanel extends JPanel {
 
@@ -258,6 +319,7 @@ class EnterDataPanel extends JPanel implements ActionListener {
             try {
                 FileWriter fr = new FileWriter(data, true);
                 fr.write("\r");
+
                 fr.write(addData.getText());
                 fr.close();
             } catch (IOException e) {
---
+++ src/RecordModel.java
@@ -0,0 +1,27 @@
+import java.util.Date;
+
+public class RecordModel {
+    private Date date;
+    private Float value;
+
+    public RecordModel(Date date, Float value) {
+        this.date = date;
+        this.value = value;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Float getValue() {
+        return value;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setValue(Float value) {
+        this.value = value;
+    }
+}
